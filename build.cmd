1a30da1d37a88e4950fe85cbfe2f3f9d5c9f627e
Bing_Yanchi <Bing_Yanchi@outlook.com>
2022-02-23 22:17:45 +0800

    新增 ico 图标的构建

--- build.cmd
+++ build.cmd
@@ -1,8 +1,9 @@
 rd/s/q build
 rd/s/q dist
 
-pyinstaller BingLocalNetChatClient.py --noconsole --icon="logo.ico"
+pyinstaller BingLocalNetChatClient.py --icon="logo.ico"
 
 copy logo.png dist\BingLocalNetChatClient
+copy logo.ico dist\BingLocalNetChatClient
 xcopy font .\dist\BingLocalNetChatClient\font /I
 xcopy style .\dist\BingLocalNetChatClient\style /I
